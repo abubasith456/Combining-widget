8c15e4e8393f692f66a977b3b66fa016471812be
abubasith456 <abubasith456@gmail.com>
2022-02-22 19:48:44 +0530

    List view

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -11,47 +11,52 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transaction.map((text) {
+      child: ListView.builder(
+          itemBuilder: (context, index) {
             return Card(
-              elevation: 5,
-              child: Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 20),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blue, width: 2),
-                    ),
-                    child: Text(
-                      'Rs.' + text.amount.toString(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.blue),
-                    ),
-                    padding: const EdgeInsets.all(10),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  elevation: 5,
+                  child: Row(
                     children: [
-                      Text(
-                        text.title,
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 20),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue, width: 2),
+                        ),
+                        child: Text(
+                          'Rs.' + text.amount.toString(),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.blue),
+                        ),
+                        padding: const EdgeInsets.all(10),
                       ),
-                      Text(
-                        DateFormat().format(text.date),
-                        style:
-                            const TextStyle(fontSize: 14, color: Colors.grey),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            text.title,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat().format(text.date),
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.grey),
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
-      ),
+                  ),
+                );
+          },
+          itemCount: transaction.length,
+          children: [
+            Column(
+              children: transaction.map((text) {
+                return 
+              }).toList(),
+            ),
+          ]),
     );
   }
 }
